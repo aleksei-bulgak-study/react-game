0823ef4434737bedfa11199479387d4bad4b9744
aleksei-bulgak-study <bulgak.aleksei@yandex.ru>
2021-03-02 19:21:49 +0000

    Deploying to gh-pages from @ aleksei-bulgak-study/react-game@8c0782b16636a9079a03aba5ba4f9d9280240e48 🚀

--- app.bundle.js
+++ app.bundle.js
@@ -49,8 +49,8 @@
         flex-direction: row;
         flex-wrap: wrap;
 
-        width: clamp(500px, 50%, 1024px);
-        height: clamp(500px, 50%, 1024px);
+        width: clamp(400px, 50%, 1024px);
+        height: clamp(400px, 50%, 1024px);
 
         background-color: ${e=>e.theme.board.background};
         border-radius: 10px;
